9f9961f901ffeb4baa7db08bef1dcc1a506abff4
Amirali Mohayaee <amig90@yahoo.com>
2022-06-15 21:19:02 +0100

    Initial commit

---
+++ MyTests/test.js
@@ -0,0 +1,20 @@
+import "nes.css/css/nes.min.css";
+
+// Enabling strict mode for more effective error checking
+'use strict'
+
+
+
+
+var num = 20;
+
+console.log(num);
+
+var message = "Hello World!";
+
+console.log(message);
+
+console.log('Hello');
+
+document.write("This will show up on html hopefully :)");
+
